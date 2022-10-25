cdd6576f38944a04f6b869685ce82636bd31053e
Matheus Alves Pereira <matttalves@gmail.com>
2022-10-25 11:40:50 -0300

    feat: comments

---
+++ fundamentals/src/comments.rs
@@ -0,0 +1,17 @@
+// https://doc.rust-lang.org/book/ch03-04-comments.html
+pub fn example() {
+  // So we’re doing something complicated here, long enough that we need
+  // multiple lines of comments to do it! Whew! Hopefully, this comment will
+  // explain what’s going on.
+  /*
+    multi
+    line
+    comment
+  */
+  println!("\n########################## comments:");
+
+  // I’m feeling lucky today
+  let lucky_number = 7; // I’m feeling lucky today
+
+  println!("Comments lucky number: {lucky_number}");
+}
--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,9 +1,11 @@
 mod variables_and_mutability;
 mod data_types;
 mod functions;
+mod comments;
 
 fn main() {
     variables_and_mutability::example();
     data_types::example();
     functions::example();
+    comments::example();
 }
