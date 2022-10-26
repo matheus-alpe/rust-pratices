9d2bd58d3ae6c5967cbc8fc6ebf9d733e05d8e3c
Matheus Alves Pereira <matttalves@gmail.com>
2022-10-26 10:58:26 -0300

    refactor: split concepts

---
+++ fundamentals/src/concepts2/ownership.rs
@@ -0,0 +1,5 @@
+// https://doc.rust-lang.org/book/ch03-02-data-types.html
+pub fn example() {
+  println!("\n########################## ownership:");
+
+}
--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,9 +1,18 @@
 mod concepts1;
+mod concepts2;
 
 fn main() {
-    concepts1::variables_and_mutability::example();
-    concepts1::data_types::example();
-    concepts1::functions::example();
-    concepts1::comments::example();
-    concepts1::control_flow::example();
+    let concept_number = 2;
+
+    if concept_number == 1 {
+        concepts1::variables_and_mutability::example();
+        concepts1::data_types::example();
+        concepts1::functions::example();
+        concepts1::comments::example();
+        concepts1::control_flow::example();
+    }
+
+    if concept_number == 2 {
+        concepts2::ownership::example();
+    }
 }
